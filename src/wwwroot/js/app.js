05343457a16f3acd2309dba4b471e30f6dfb7799
Julien Desrosiers <juliend2@gmail.com>
2026-03-23 16:39:43 -0400

    allow editing content

--- src/wwwroot/js/app.js
+++ src/wwwroot/js/app.js
@@ -1,11 +1,14 @@
 const letterField = document.querySelector("#letter")
 const numberField = document.querySelector("#number")
+const contentField = document.querySelector("#content")
 
 const cells = document.querySelectorAll(".js-cell")
+
 cells.forEach(c => {
     c.addEventListener("click", (e) => {
         e.stopPropagation()
         letterField.value = String(e.target.dataset.letter).toUpperCase()
         numberField.value = e.target.dataset.number
+        contentField.innerHTML = e.target.innerHTML.trim()
     })
 })
